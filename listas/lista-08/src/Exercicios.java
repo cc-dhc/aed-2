bc13730576e002a9d03640e260dc80c4496c33d5
Pedro Santayana <me@pedrosantayana.dev>
2024-04-30 16:52:10 -0300

    todo lista 08

---
+++ listas/lista-08/src/Exercicios.java
@@ -0,0 +1,197 @@
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    FilaSemCabeca fila = new FilaSemCabeca();
+
+    fila.enfileirar(10);
+    fila.enfileirar(12);
+    fila.enfileirar(13);
+    fila.enfileirar(14);
+
+    fila.mostrar();
+
+    fila.remover();
+    fila.mostrar();
+
+    System.out.println();
+    //fila.mostrar();
+    //System.out.println(fila.menor());
+  }
+}
+
+class No {
+  private int element;
+  private No nextNode;
+
+  public No(int element, No nextNode) {
+    this.element = element;
+    this.nextNode = nextNode;
+  }
+
+  public int getElement() {
+    return element;
+  }
+
+  public void setElement(int element) {
+    this.element = element;
+  }
+
+  public No getNextNode() {
+    return nextNode;
+  }
+
+  public void setNextNode(No nextNode) {
+    this.nextNode = nextNode;
+  }
+}
+
+class Fila {
+  private No head;
+
+  public Fila() {
+    head = new No(0, null);
+  }
+
+  public void enfileirar(int element) {
+    No current = head;
+
+    while (current.getNextNode() != null)
+      current = current.getNextNode();
+
+    current.setNextNode(new No(element, null));
+  }
+
+  public int remover() {
+    No first = head.getNextNode();
+
+    if (first != null)
+      head = first;
+
+    return first.getElement();
+  }
+
+  public void mostrar() {
+    No current = head;
+
+    while (current.getNextNode() != null) {
+      current = current.getNextNode();
+      System.out.println(current.getElement());
+    }
+
+  }
+
+  public int amplitude() {
+    No current = head;
+    int maior = Integer.MIN_VALUE, menor = Integer.MAX_VALUE;
+
+    while (current.getNextNode() != null) {
+      current = current.getNextNode();
+      if (current.getElement() > maior)
+        maior = current.getElement();
+      if (current.getElement() < menor)
+        menor = current.getElement();
+    }
+
+    return maior - menor;
+  }
+
+  public double media() {
+    No current = head;
+    double sum = 0;
+    int count = 0;
+
+    while (current.getNextNode() != null) {
+      current = current.getNextNode();
+      sum += current.getElement();
+      count++;
+    }
+
+    return sum / (double) count;
+  }
+
+  public void fura(int e, int pos) {
+    No prev = head;
+    No next = head.getNextNode();
+    int currentPos = 0;
+
+    while (currentPos != pos && next != null) {
+      prev = next;
+      next = next.getNextNode();
+      currentPos++;
+    }
+
+    No novo = new No(e, next);
+    prev.setNextNode(novo);
+  }
+
+  public int menor() {
+    if (head.getNextNode() == null) throw new RuntimeException("Fila Vazia");
+
+    return menor(head.getNextNode());
+  }
+
+  private int menor(No no) {
+    int menor = Integer.MAX_VALUE;
+
+    if (no == null)
+      return menor;
+
+    menor = menor(no.getNextNode());
+
+    return Math.min(no.getElement(), menor);
+  }
+
+  public void inverter() {
+    if (head.getNextNode() == null)
+      return;
+
+    No anterior = head;
+    anterior.setNextNode(null);
+    No proximo = head.getNextNode();
+
+    while (proximo.getNextNode() != null) {
+
+    }
+
+  }
+}
+
+class FilaSemCabeca {
+  private No fila;
+
+  public FilaSemCabeca() {
+    this.fila = null;
+  }
+
+  public void enfileirar(int element) {
+    if (fila == null) {
+      fila = new No(element, null);
+      return;
+    }
+
+    No current = fila.getNextNode();
+
+    while (current.getNextNode() != null) current = current.getNextNode();
+
+    current.setNextNode(new No(element, null));
+  }
+
+  public int remover() {
+    if (fila == null) throw new RuntimeException("Fila Vazia");
+
+    No first = fila;
+    fila = fila.getNextNode();
+
+    return first.getElement();
+  }
+
+  public void mostrar() {
+    No current = fila;
+
+    while (current != null) {
+      System.out.println(current.getElement());
+      current = current.getNextNode();
+    }
+
+  }
+}
